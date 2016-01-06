92dce9e205779e0995222a09f3d24f8055fe728c
uhmin <uhming@gmail.com>
2016-01-06 12:12:52 +0900

    Update installing-BioPerl.sh

--- installing-BioPerl.sh
+++ installing-BioPerl.sh
@@ -10,14 +10,20 @@ echo "Installing perl modules"
 sudo yum -y install perl-CPAN expat21-devel perl-DBD-MySQL gd perl-IO-Compress-Bzip2 \
     perl-XML-Parser perl-libxml-perl perl-libxml-perl perl-GraphViz \
     perl-Archive-Tar perl-Archive-Zip perl-Text-Glob perl-Test-MockModule \
-    perl-ExtUtils-CBuilder perl-IO-Compress-Base perl-Module-Build # for bioperl
+    perl-ExtUtils-CBuilder perl-IO-Compress-Base perl-Module-Build \
+    perl-Test-Most perl-Exception-Class perl-Class-Data-Inheritable \
+    perl-Capture-Tiny perl-ExtUtils-* perl-version perl-CPAN-Meta-YAML # for bioperl
 
-echo "Installing BioPerl"
+echo "Setup for installing BioPerl"
 sudo perl -MCPAN -e shell <<EOF
 install Bundle::CPAN
 install Module::Build
 o conf prefer_installer MB
 o conf commit
+EOF
+
+echo "Installing BioPerl"
+sudo perl -MCPAN -e shell <<EOF
 force install CJFIELDS/BioPerl-1.6.924.tar.gz
 EOF
 
